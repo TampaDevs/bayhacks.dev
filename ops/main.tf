4190ec94001c6651684481c600c85bb403de462d
Douglas Moore <doug.moore@oddball.io>
2023-05-15 11:58:57 -0400

    fix terraform issue

--- ops/main.tf
+++ ops/main.tf
@@ -13,7 +13,7 @@ terraform {
 }
 
 locals {
-  name = "bayhacks.codabool.com"
+  name = "bayhacks.dev"
 }
 
 resource "aws_s3_bucket" "main" {
@@ -21,41 +21,29 @@ resource "aws_s3_bucket" "main" {
   force_destroy = true
 }
 
+data "aws_iam_policy_document" "public_read" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.main.arn}/*"]
+  }
+}
+
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.main.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid = "PublicReadGetObject",
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.main.arn}/*",
-        # Condition = {
-        #   IpAddress = {
-        #     "aws:SourceIp" = [
-        #       "173.245.48.0/20",
-        #       "103.21.244.0/22",
-        #       "103.22.200.0/22",
-        #       "103.31.4.0/22",
-        #       "141.101.64.0/18",
-        #       "108.162.192.0/18",
-        #       "190.93.240.0/20",
-        #       "188.114.96.0/20",
-        #       "197.234.240.0/22",
-        #       "198.41.128.0/17",
-        #       "162.158.0.0/15",
-        #       "104.16.0.0/13",
-        #       "104.24.0.0/14",
-        #       "172.64.0.0/13",
-        #       "131.0.72.0/22"
-        #     ]
-        #   }
-        # }
-      }
-    ]
-  })
+  policy = data.aws_iam_policy_document.public_read.json
+}
+
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.main.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "main" {
@@ -89,3 +77,44 @@ resource "aws_s3_object" "object" {
   # TODO: make modular
   etag         = filemd5("../dist/${each.value}")
 }
+
+
+# Cloudflare CIDRs
+
+
+# resource "aws_s3_bucket_policy" "public_read" {
+#   bucket = aws_s3_bucket.main.id
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid = "PublicReadGetObject",
+#         Effect = "Allow",
+#         Principal = "*",
+#         Action = "s3:GetObject",
+#         Resource = "${aws_s3_bucket.main.arn}/*"
+#       }
+#     ]
+#   })
+#         # Condition = {
+#         #   IpAddress = {
+#         #     "aws:SourceIp" = [
+#         #       "173.245.48.0/20",
+#         #       "103.21.244.0/22",
+#         #       "103.22.200.0/22",
+#         #       "103.31.4.0/22",
+#         #       "141.101.64.0/18",
+#         #       "108.162.192.0/18",
+#         #       "190.93.240.0/20",
+#         #       "188.114.96.0/20",
+#         #       "197.234.240.0/22",
+#         #       "198.41.128.0/17",
+#         #       "162.158.0.0/15",
+#         #       "104.16.0.0/13",
+#         #       "104.24.0.0/14",
+#         #       "172.64.0.0/13",
+#         #       "131.0.72.0/22"
+#         #     ]
+#         #   }
+#         # }
+# }
